9bb70c0be2f3a2480d4e43462002ae10709ed68d
daw2-ramudo22 <91276092+daw2-ramudo22@users.noreply.github.com>
2021-10-27 17:07:18 +0200

    Add files via upload

---
+++ N3P206comments.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+ <meta charset="utf-8">
+        <title>Comentario Practica 4</title>
+</head>
+<body>
+	<ol>
+		<li>Conectarse al server SQL mediante PHP</li>
+		<li>Crear tablas con PHP</li>
+		<li>Hacer querys en PHP</li>
+		<li>Foreach</li>
+		<li>Seleccionar database</li>
+	</ol>
+	<?php
+        echo "<p>Califica el documento y la explicacion del profe del 1 al 10: </p>";
+	echo rand(1, 10);
+	echo "<p>Como estudiante, calificate: </p>";
+	echo rand(1, 10);
+
+            ?>
+
+</body>
+</html>
